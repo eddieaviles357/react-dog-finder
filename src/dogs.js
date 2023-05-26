828fed3545712a81d7c624fdc32da1d2c6132ea9
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-25 21:19:15 -0700

    DOGS object image urls added

---
+++ src/dogs.js
@@ -0,0 +1,8 @@
+let DOGS = {
+    whiskey: 'https://cdn.pixabay.com/photo/2016/10/10/14/13/dog-1728494_1280.png',
+    duke: 'https://cdn.pixabay.com/photo/2015/11/17/13/13/bulldog-1047518_1280.jpg',
+    perry: 'https://cdn.pixabay.com/photo/2012/05/07/13/41/dog-48490_1280.png',
+    tubby: 'https://cdn.pixabay.com/photo/2018/05/26/18/06/dog-3431913_1280.jpg'
+}
+
+export default DOGS;
