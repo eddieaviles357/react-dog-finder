429d92a95af974bdfbbf7a5b11c0fa901c7b1776
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-25 21:27:55 -0700

    Navigation for dogs

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { NavLink } from 'react-router-dom';
 
-export default function Nav() {
+export default function Nav({dogs}) {
+    // keys should not be index use uuid npm instead
+    const dogNavLinks = dogs.map((dog, idx)=><li key={idx}><NavLink exact to={`/dogs/${dog.name}`}>{dog.name}</NavLink></li>)
+
     return (
-        <nav>
-            <NavLink exact to='/dogs'>Dogs</NavLink>
+        <nav className='Nav'>
+            <h1>Dogs</h1>
+            <ul>
+            {dogNavLinks}
+            </ul>
         </nav>
     )
 }
