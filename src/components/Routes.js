494e1ad7adfd9f4e7a8f6bb10f12b34cc12f61ee
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-25 21:28:23 -0700

    updated routes to include Nav component that will be rendered as /dogs

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import Nav from './Nav';
 
-export default function Routes() {
+export default function Routes({dogs}) {
     return (
         <Switch>
-            <Route exact path='/dogs' ></Route>
-            <Route exact path='/dogs/:dog' ></Route>
+            <Route exact path='/dogs'>
+                <Nav dogs={dogs}/>
+            </Route>
+            <Route exact path='/dogs/:dog'>
+                
+            </Route>
             <Redirect to='/dogs' />
         </Switch>
     )
