ab1febc1ac1efba6ba9ed5e19978a9f33c7c3201
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-26 18:58:00 -0700

    Routes will now hold the dogs state

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Nav from './Nav';
+import DogDetails from './DogDetail';
+import dogs from '../dogs';
 
-export default function Routes({dogs}) {
+export default function Routes() {
+    const [dogState] = useState(dogs);
+
+    const getDog = (dogName) => {
+        const foundDog = dogState.find(dogs => dogs['name'] === dogName)
+        return foundDog;
+    };
+    
     return (
         <Switch>
             <Route exact path='/dogs'>
-                <Nav dogs={dogs}/>
+                <Nav dogs={dogState}/>
             </Route>
             <Route exact path='/dogs/:dog'>
-                
+                <DogDetails getDog={getDog} />
             </Route>
             <Redirect to='/dogs' />
         </Switch>
