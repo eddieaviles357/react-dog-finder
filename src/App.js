25f83397a9c058521349915936287613af610a4d
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-26 18:57:34 -0700

    removed default props to its own file and removed dogs object to its own file

--- src/App.js
+++ src/App.js
@@ -1,60 +1,12 @@
 import './App.css';
 import Routes from './components/Routes';
-import DOGS from './dogs';
 
-const { whiskey, duke, perry, tubby } = DOGS;
-
-function App(props) {
+function App() {
   return (
     <div className="App">
-      <Routes dogs={props.dogs}/>
+      <Routes />
     </div>
   );
-}
-
-App.defaultProps = {
-  dogs: [
-    {
-      name: "Whiskey",
-      age: 5,
-      src: whiskey,
-      facts: [
-        "Whiskey loves eating popcorn.",
-        "Whiskey is a terrible guard dog.",
-        "Whiskey wants to cuddle with you!"
-      ]
-    },
-    {
-      name: "Duke",
-      age: 3,
-      src: duke,
-      facts: [
-        "Duke believes that ball is life.",
-        "Duke likes snow.",
-        "Duke enjoys pawing other dogs."
-      ]
-    },
-    {
-      name: "Perry",
-      age: 4,
-      src: perry,
-      facts: [
-        "Perry loves all humans.",
-        "Perry demolishes all snacks.",
-        "Perry hates the rain."
-      ]
-    },
-    {
-      name: "Tubby",
-      age: 4,
-      src: tubby,
-      facts: [
-        "Tubby is really stupid.",
-        "Tubby does not like walks.",
-        "Angelina used to hate Tubby, but claims not to anymore."
-      ]
-    }
-  ]
 }
 
 export default App;
