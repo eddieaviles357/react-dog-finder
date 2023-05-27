b822f19b4216502758d3b26d65ee40e4d30917fe
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-26 18:58:25 -0700

    added urls to dogs object

--- src/dogs.js
+++ src/dogs.js
@@ -1,8 +1,44 @@
-let DOGS = {
-    whiskey: 'https://cdn.pixabay.com/photo/2016/10/10/14/13/dog-1728494_1280.png',
-    duke: 'https://cdn.pixabay.com/photo/2015/11/17/13/13/bulldog-1047518_1280.jpg',
-    perry: 'https://cdn.pixabay.com/photo/2012/05/07/13/41/dog-48490_1280.png',
-    tubby: 'https://cdn.pixabay.com/photo/2018/05/26/18/06/dog-3431913_1280.jpg'
-}
 
-export default DOGS;
+const dogs = [
+    {
+      name: "Whiskey",
+      age: 5,
+      src: 'https://cdn.pixabay.com/photo/2016/10/10/14/13/dog-1728494_1280.png',
+      facts: [
+        "Whiskey loves eating popcorn.",
+        "Whiskey is a terrible guard dog.",
+        "Whiskey wants to cuddle with you!"
+      ]
+    },
+    {
+      name: "Duke",
+      age: 3,
+      src: 'https://cdn.pixabay.com/photo/2015/11/17/13/13/bulldog-1047518_1280.jpg',
+      facts: [
+        "Duke believes that ball is life.",
+        "Duke likes snow.",
+        "Duke enjoys pawing other dogs."
+      ]
+    },
+    {
+      name: "Perry",
+      age: 4,
+      src: 'https://cdn.pixabay.com/photo/2012/05/07/13/41/dog-48490_1280.png',
+      facts: [
+        "Perry loves all humans.",
+        "Perry demolishes all snacks.",
+        "Perry hates the rain."
+      ]
+    },
+    {
+      name: "Tubby",
+      age: 4,
+      src: 'https://cdn.pixabay.com/photo/2018/05/26/18/06/dog-3431913_1280.jpg',
+      facts: [
+        "Tubby is really stupid.",
+        "Tubby does not like walks.",
+        "Angelina used to hate Tubby, but claims not to anymore."
+      ]
+    }
+  ]
+export default dogs;
